226f905da705d24bb54243f0e2681c2280f18954
umoarsman <nesg0003@gmail.com>
2025-07-12 15:01:44 -0500

    001_added fps counter

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
@@ -19,6 +22,9 @@ def main():
 
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        time_passed = game_clock.tick(60)
+        dt = time_passed / 1000
 
 if __name__ == "__main__":
     main()
